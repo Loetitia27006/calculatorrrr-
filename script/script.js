858ec44c7b35189c7d008ef91c6934353ea80fbc
loetitia <audiger.loetitia@gmail.com>
2022-06-23 18:37:18 +0200

    debut js

---
+++ script/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var screen = document.querySelector(".screen")
+    var simpleButtons = document.querySelectorAll(".btn-number, . btn-operator, #btn-dot"
+    )
+
+    for(var button of simpleButtons){
+        var newChar = this.textContent
+        var currentText = screen.textContent
+
+        
+           
+           
+           
+        
+    }
+})
